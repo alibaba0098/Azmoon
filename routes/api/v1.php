b81275c8d7c858f6ed2e3ea6e9b7a68d246080ad
Erfan <efiz.0098@gmail.com>
2024-08-04 11:02:21 +0330

    versioning implemented

---
+++ routes/api/v1.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/api/v1', function () {
+    echo "Hello from v1";
+});
---
+++ routes/api/v2.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/api/v2', function () {
+    echo "Hello from v2";
+});
